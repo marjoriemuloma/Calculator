740a3f51f0357c0869a3b7babea54c710afe774c
marjoriemuloma <52866514+marjoriemuloma@users.noreply.github.com>
2019-08-24 12:27:00 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,49 @@
+# This program is a simple calculator
+# We shall develop it further as we go on with the project
+
+
+
+name = str(input("What is your name?: "))
+
+operation = str(
+    input(
+        "Type add for addition sub for subtraction and mul for multiplication: "
+    )
+)
+
+if operation == 'add':
+    print("Welcome to the calculator " + name)
+    print("This is a simple calculator that adds two integers... enjoy!")
+
+    num1 = int(input("Enter an integer: "))
+    num2 = int(input("Enter another integer: "))
+
+    result = num1 + num2
+
+    print("The answer is " + str(result))
+
+elif operation == 'sub':
+    print("Welcome to the calculator " + name)
+    print("This is a simple calculator that subtracts two integers... enjoy!")
+
+    num1 = int(input("Enter an integer: "))
+    num2 = int(input("Enter another integer: "))
+
+    result = num1 - num2
+
+    print("The answer is " + str(result))
+
+
+elif operation == 'mul':
+        print("Welcome to the calculator " + name)
+        print("This is a simple calculator that multiplies two integers... enjoy!")
+
+        num1 = int(input("Enter an integer: "))
+        num2 = int(input("Enter another integer: "))
+
+        result = num1 * num2
+
+        print("The answer is " + str(result))
+
+else:
+    print("The operation does not exist... Try again.")
